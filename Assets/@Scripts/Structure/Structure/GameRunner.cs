e8708eead4058a422236b8f2ea327c015d4544d1
boss.timysn94@outlook.com <boss.timysn94@outlook.com>
2023-03-16 11:07:17 +0300

    - Added game runner to init the game scene (foe editor) and a loading curtain.

---
+++ Assets/@Scripts/Structure/Structure/GameRunner.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Defender.Structure
+{
+    public class GameRunner : MonoBehaviour
+    {
+        public Bootstrap BootPrefab;
+
+        private void Awake()
+        {
+            var bootstrap = FindObjectOfType<Bootstrap>();
+
+            if (bootstrap == null)
+                Instantiate(BootPrefab);
+        }
+    }
+}
