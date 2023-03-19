ece160f52fc2384edd88ec1075d9e5aeec7c19f1
boss.timysn94@outlook.com <boss.timysn94@outlook.com>
2023-03-19 23:46:57 +0300

    - Added comfort spawner marker distributor.

---
+++ Assets/@Scripts/Editor/CircleObjectDistributorEditor.cs
@@ -0,0 +1,21 @@
+﻿using Defender.Utility;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(CircleObjectDistributor))]
+    public class CircleObjectDistributorEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (GUILayout.Button("Distribute Objects"))
+            {
+                CircleObjectDistributor circleDistributor = (CircleObjectDistributor)target;
+                circleDistributor.UpdateObjectPositions();
+            }
+        }
+    }
+}
---
+++ Assets/@Scripts/Utility/CircleObjectDistributor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Defender.Utility
+{
+    public class CircleObjectDistributor : MonoBehaviour
+    {
+        public float Radius = 5f;
+        public List<Transform> Objects = new List<Transform>();
+
+        private void OnValidate()
+        {
+            UpdateObjectPositions();
+        }
+
+        public void UpdateObjectPositions()
+        {
+            if (Objects.Count == 0)
+            {
+                return;
+            }
+
+            float angle = 360f / Objects.Count;
+            float currentAngle = 0f;
+
+            for (int i = 0; i < Objects.Count; i++)
+            {
+                Vector2 objectPosition = new Vector2(Mathf.Cos(currentAngle * Mathf.Deg2Rad), Mathf.Sin(currentAngle * Mathf.Deg2Rad)) * Radius;
+                Objects[i].position = (Vector2)transform.position + objectPosition;
+                currentAngle += angle;
+            }
+        }
+    }
+}
